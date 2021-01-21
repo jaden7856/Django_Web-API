25d80685dec9d9622457dd581167a0c1192f1063
Jaden7856 <jhg7856@gmail.com>
2021-01-21 17:50:39 +0900

    Django를 활용하여 Site기초설계

---
+++ README.md
@@ -0,0 +1,3 @@
+# Blog DB API
+
+![Blog_DB_API](README.assets/Blog_DB_API.PNG)
---
+++ blog/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField('TITLE', max_length=50)
+    slug = models.SlugField('SLUG', unique=True, allow_unicode=True, help_text='one word for this alias')
+    description = models.CharField('DESCRIPTION', max_length=100, blank=True, help_text='simple description text')
+    content = models.TextField('CONTENT')
+    create_date = models.DateTimeField('Create Date', auto_now_add=True)
+    modify_date = models.DateTimeField('Modify Date', auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'post'
+        verbose_name_plural = 'posts'
+        db_table = 'blog_posts'
+        #ordering = ('modify_date', 'author')     # asc & author
+        ordering = ('-modify_date',)     # desc
+
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=(self.slug,)) # -> 1st post -> /blog/post/1st-post
+
+    def get_previous_post(self):
+        return self.get_previous_by_modify_date()
+
+    def get_next_post(self):
+        return self.get_next_by_modify_date()
+    
